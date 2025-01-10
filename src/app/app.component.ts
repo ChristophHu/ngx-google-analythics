757c74e3d372ec49990e4074d7149ec728360241
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-01-10 12:40:39 +0100

    adding ghpages, update routs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { AnalythicsService } from './core/services/analythics/analythics.service';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { IconsComponent } from './shared/components/icons/icons.component';
+import { GithubService } from './core/services/github/github.service';
+import { Observable } from 'rxjs';
 
 declare const gtag: Function
 
 @Component({
   selector: 'app-root',
   imports: [
+    AsyncPipe,
     CommonModule,
-    RouterOutlet
+    IconsComponent,
+    RouterModule
   ],
   providers: [
-    AnalythicsService
+    AnalythicsService,
+    GithubService
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent implements OnInit {
-  title = 'ngx-google-analythics';
 
-  constructor(public router: Router, private _analythicsService: AnalythicsService) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'MEASUREMENT-ID', { 'page_path': event.urlAfterRedirects });
-      }      
-    })
+  constructor(public router: Router, private _analythicsService: AnalythicsService, private _githubService: GithubService) {
+    this.repos$ = this._githubService.repos$
+    this.this_repo$ = this._githubService.this_repo$
+    this.user$ = this._githubService.user$
+
+    this.name = this._githubService.getThisRepo()
+    this.version = this._githubService.getVersion()
+
+    // this.router.events.subscribe((event) => {
+    //   if (event instanceof NavigationEnd) {
+    //     gtag('config', 'MEASUREMENT-ID', { 'page_path': event.urlAfterRedirects });
+    //   }      
+    // })
   }
 
   ngOnInit(): void {
-    this._analythicsService.trackEvent('component_loaded', 'Component loaded into view', 'initializing');
+    // this._analythicsService.trackEvent('component_loaded', 'Component loaded into view', 'initializing');
+  }
+
+  // github-service
+  repos$: Observable<any>
+  this_repo$: Observable<any>
+  user$: Observable<any>
+
+  show_settings: boolean = false
+  name: string = ''
+  version: string = '0.0.1'
+  
+  toggleSettings() {
+    this.show_settings = !this.show_settings
+  }
+  toggleTheme() {
+    this.toggleSettings()
   }
 }
