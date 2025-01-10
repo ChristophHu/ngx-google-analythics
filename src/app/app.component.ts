459a425cd38b215e23f340260eb8fd028a2f0583
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-01-10 11:21:37 +0100

    add service and google-tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnalythicsService } from './core/services/analythics/analythics.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
+  providers: [
+    AnalythicsService
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngx-google-analythics';
+
+  constructor(private _analythicsService: AnalythicsService) {}
+
+  ngOnInit(): void {
+    this._analythicsService.trackEvent('component_loaded', 'Component loaded into view', 'initializing');
+  }
 }
